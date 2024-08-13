50fe686467331392f2f933d35ee0bd16eeae0ef2
Rod97139 <fayaflame@gmail.com>
2024-08-13 21:37:17 +0200

    update modal

--- src/components/home/reactModal/ReactModal.jsx
+++ src/components/home/reactModal/ReactModal.jsx
@@ -1,5 +1,4 @@
 import Modal from 'react-modal';
-
 const ReactModal = ({shouldOpenModal, setShouldOpenModal}) => {
 
     const customStyles = {
@@ -9,7 +8,14 @@ const ReactModal = ({shouldOpenModal, setShouldOpenModal}) => {
           right: 'auto',
           bottom: 'auto',
           marginRight: '-50%',
+          width: '300px',
+          height: '70px',
           transform: 'translate(-50%, -50%)',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+
         },
       };
 
@@ -28,8 +34,8 @@ const ReactModal = ({shouldOpenModal, setShouldOpenModal}) => {
         style={customStyles}
         contentLabel="Example Modal"
         >
-          <div>Employee correctly added</div>
-          <button onClick={closeModal}>close</button>
+          <div className='modal-texte'>Employee added successfully</div>
+          <button className='hr-btn' onClick={closeModal}>close</button>
         </Modal>
     </div>
   )
