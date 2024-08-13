031fce0b7e7aa0a4fa81254b9515d6c66b84d9b6
Rod97139 <fayaflame@gmail.com>
2024-08-13 02:11:07 +0200

    pagination

--- src/components/employee-list/employeeTable.jsx/EmployeeTable.jsx
+++ src/components/employee-list/employeeTable.jsx/EmployeeTable.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux"
 import { getEmployeeList } from "../../../selectors";
-import ReactDataTable from "./ReactDataTable";
+import {ReactDataTable} from "react-data-table-by-rod-test-v2";
+import './ReactDataTable.css';
 
 const EmployeeTable = () => {
     const employees = useSelector(getEmployeeList)
+    
     return (
         <ReactDataTable data={employees} />
     )
