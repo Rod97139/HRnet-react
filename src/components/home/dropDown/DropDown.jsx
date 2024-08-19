07eb700d7b35ae26f9aff6883dafb8f16593f675
Rod97139 <fayaflame@gmail.com>
2024-08-19 21:22:57 +0200

    react-select custom dropdown

--- src/components/home/dropDown/DropDown.jsx
+++ src/components/home/dropDown/DropDown.jsx
@@ -1,13 +1,27 @@
+
+import Select from 'react-select'
+
 const DropDown = ({ name, content, setInput }) => {
 
-
+    const options = name === "state" ? content.map((item) => ({ value: item.abbreviation, label: item.name })) 
+                                        : content.map((item) => ({ value: item, label: item }));
+    
     return (
-         <select className={`dropdown-${name}`} name={name} id={name} onChange={(e) => setInput(e.target.value)}>
-            {name === "state" 
-                ? content.map((item, index) => <option key={index} value={item.abbreviation}>{item.name}</option>) 
-                : content.map((item, index) => <option key={index}>{item}</option>)
-            }
-        </select>
+        <Select
+            theme={(theme) => ({
+                ...theme,
+                colors: {
+                ...theme.colors,
+                primary25: '#93ad18',
+                primary: '#5a6f08',
+                },
+            })}
+            className={`dropdown-${name}`} 
+            onChange={(e) => setInput(e.value)}  
+            options={options} 
+            placeholder={options[0].label} 
+            defaultValue={options[0].value} 
+         />
     )
 }
 
